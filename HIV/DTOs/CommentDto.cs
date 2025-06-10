6dbeedddc04af2f7549f0686a50318b3e592c226
Tho0205 <tho02052004@gmail.com>
2025-06-10 08:57:13 +0700

    Upd Comment

---
+++ HIV/DTOs/CommentDto.cs
@@ -0,0 +1,13 @@
+﻿namespace HIV.DTOs
+{
+    public class CommentDto
+    {
+        public int CommentId { get; set; }
+        public int BlogId { get; set; }
+        public int UserId { get; set; }
+        public string? Content { get; set; }
+        public string Status { get; set; } = "ACTIVE";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string? User { get; set; }
+    }
+}
